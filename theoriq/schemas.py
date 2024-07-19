2deec7f88024f3334a1343ee7da633147cc94303
Jérémy d'Auria <jeremydauria@chainml.net>
2024-07-19 15:56:00 +0200

    doc: Update and enhance schema documentation

--- theoriq/schemas.py
+++ theoriq/schemas.py
@@ -1,17 +1,42 @@
-from pydantic import BaseModel
+"""
+schemas.py
 
-from theoriq.types import RequestBiscuit
+This module contains the schemas used by the theoriq endpoint.
+"""
+
+from pydantic import BaseModel
 
 
 class DialogItemBlock(BaseModel):
-    """Schema for dialog item block."""
+    """
+    Represents a specific dialogue item block within a DialogItem.
+
+    A DialogItemBlock is an individual component in a ConversationItem's list of items.
+    Each block contains data of a specific type which could be one of several formats such as text, markdown or code.
+
+    Attributes:
+        type (str): Specifies the format of the text in the block.
+        data (str): Contains the actual response in the specified format.
+    """
 
     type: str
     data: str
 
 
 class DialogItem(BaseModel):
-    """Schema for dialog item."""
+    """
+    A DialogItem object represents a message from a source during a dialog.
+
+    A single DialogItem contains multiple instances of DialogItemBlock. This allows an agent to send
+    multi-format responses for a single request. An agent, for example, can send Python and SQL code blocks
+    along with a markdown block.
+
+    Attributes:
+        timestamp (str): The creation time of the dialog item.
+        source (str): The creator of the dialog item. In the agent context, this is the agent's ID in the Theoriq protocol.
+        sourceType (str): The type of the source that creates the dialog item. Can be either 'user' or 'agent'.
+        items (list[DialogItemBlock]): A list of DialogItemBlock objects consisting of responses from the agent.
+    """
 
     timestamp: str
     source: str
@@ -20,25 +45,34 @@ class DialogItem(BaseModel):
 
 
 class ExecuteRequestBody(BaseModel):
+    """
+    Represents the expected payload for an execute request.
+
+    Attributes:
+        items (list[DialogItemBlock]): A list of DialogItemBlock objects consisting of request/response from the user and agent.
+    """
     items: list[DialogItem]
 
 
-class ExecuteRequest:
-    """Request used when calling the `execute` endpoint"""
-
-    def __init__(self, body: ExecuteRequestBody, biscuit: RequestBiscuit):
-        self.body = body
-        self.biscuit = biscuit
-
-
 class ChallengeRequestBody(BaseModel):
-    """Schema for a challenge request."""
+    """
+    Represents the expected payload for a challenge request.
+
+    Attributes:
+        nonce (str): Hex encoded string containing the challenge nonce that needs to be signed by the agent.
+    """
 
     nonce: str
 
 
 class ChallengeResponseBody(BaseModel):
-    """Schema for a challenge request."""
+    """
+    Represents the response payload for a challenge request.
+
+    Attributes:
+        nonce (str): Hex encoded string containing the nonce that has been signed by the agent.
+        signature (str): Hex encoded string containing the challenge signature.
+    """
 
     nonce: str
     signature: str
