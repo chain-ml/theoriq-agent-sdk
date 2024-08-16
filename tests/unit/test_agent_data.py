f437bcc0aca5e8b6f3f4e5b0d35ab32596625c66
Arnaud Flament <arnaud.flament@gmail.com>
2024-08-15 18:12:47 -0700

    Update UT

--- tests/unit/test_agent_data.py
+++ tests/unit/test_agent_data.py
@@ -2,6 +2,6 @@ from theoriq.types import AgentDataObject
 
 
 def test_agent_data():
-    ad = AgentDataObject.from_yaml("../agent.yaml")
+    ad = AgentDataObject.from_yaml("../data/agent.yaml")
     assert ad.spec.urls.end_point == "http://127.0.0.1:8000/"
     assert ad.metadata.name == "AwesumSum"
