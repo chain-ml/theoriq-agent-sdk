8ca12486a1222eac0ed12582b437d1d56ea6dd4e
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2024-08-30 09:34:53 -0700

    Update test_agent_address.py

--- tests/unit/test_agent_address.py
+++ tests/unit/test_agent_address.py
@@ -3,6 +3,10 @@ from theoriq.biscuit import AgentAddress
 
 
 def test_agent_address_from_public_key():
+    # Expected value is double checked with external source
+    # https://emn178.github.io/online-tools/keccak_256.html
+    # Make sure set Input and Output encoding to Hex
+    
     pub_key = PublicKey.from_hex("81d0ff760a8a2a5b3da3d942deef217fdbefe00e36106052f9194661c234777c")
     agent_address = AgentAddress.from_public_key(pub_key)
 
