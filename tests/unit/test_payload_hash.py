51a0948f1ca19999326be24eb7520ac14cd2ced5
Arnaud Flament <17051690+aflament@users.noreply.github.com>
2024-10-24 18:31:43 -0700

    Enhance biscuit error message (#35)
    * Enhance biscuit error meaage
    
    * Add class to handle PayloadHash
    
    * fix lint
    
    * Update
    
    * Update payload_hash.py
    
    * Update utils.py

---
+++ tests/unit/test_payload_hash.py
@@ -0,0 +1,38 @@
+import pytest
+from theoriq.biscuit import PayloadHash
+
+
+def test_equality():
+    payload = "Theoriq SDK helps to develop agent on top of Theoriq Protocol"
+    payload_bytes = payload.encode("utf-8")
+    ph = PayloadHash(payload=payload_bytes)
+
+    assert ph == PayloadHash.from_str(payload)
+
+    ph_str = f"{ph}"
+    assert ph == ph_str
+    assert ph == ph_str.removeprefix("0x")
+    assert ph == ph_str.upper()
+    assert ph == ph_str.lower()
+
+    hash_value = "d3c3945ef8015911102145011bb2b2d3bb31bd784aa8b19b38ad168a92777a15"
+    assert ph == PayloadHash.from_hash(hash_value)
+    assert ph == PayloadHash.from_hash(f"0x{hash_value}")
+    assert ph == PayloadHash.from_hash(f"0x{hash_value.upper()}")
+    assert ph == PayloadHash.from_hash(f"{hash_value.upper()}")
+
+
+def test_inequality():
+    payload = "Theoriq SDK helps to develop agent on top of Theoriq Protocol"
+    payload_bytes = payload.encode("utf-8")
+    ph = PayloadHash(payload=payload_bytes)
+
+    assert ph != PayloadHash.from_str(payload.upper())
+
+
+def test_invalid_hash():
+    with pytest.raises(ValueError):
+        PayloadHash.from_hash("0x")
+
+    with pytest.raises(ValueError):
+        PayloadHash.from_hash("z3c3945ef8015911102145011bb2b2d3bb31bd784aa8b19b38ad168a92777a15")
---
+++ theoriq/biscuit/payload_hash.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+import hashlib
+import re
+from typing import Any, ClassVar
+
+
+class PayloadHash:
+    _SHA256_REGEX: ClassVar[re.Pattern] = re.compile(r"^[a-fA-F0-9]{64}$")
+
+    def __init__(self, payload: bytes) -> None:
+        """
+        Initialize the PayloadHash with the given payload and compute its hash.
+        """
+        self._hash = self.compute_hash(payload)
+
+    @staticmethod
+    def compute_hash(payload: bytes) -> str:
+        """
+        Compute the SHA-256 hash of the given payload and return it as a hex string.
+        """
+        return hashlib.sha256(payload).hexdigest()
+
+    @staticmethod
+    def _normalize_hash(hash_value: str) -> str:
+        """
+        Normalize the hash by removing the '0x' prefix (if present) and converting to lowercase.
+        """
+        return hash_value.lower().removeprefix("0x")
+
+    def __eq__(self, other: Any) -> bool:
+        """
+        Compare two PayloadHash objects or a PayloadHash object with a string.
+        This comparison is case-insensitive and ignores the '0x' prefix.
+        """
+        if isinstance(other, PayloadHash):
+            return self._normalize_hash(self._hash) == self._normalize_hash(other._hash)
+        elif isinstance(other, str):
+            return self._normalize_hash(self._hash) == self._normalize_hash(other)
+        return False
+
+    def __repr__(self) -> str:
+        """
+        Return the string representation of the PayloadHash object.
+        """
+        return f"PayloadHash(hash='0x{self._hash}')"
+
+    def __str__(self) -> str:
+        """
+        Return the hash as a string in '0x' prefixed format.
+        """
+        return f"0x{self._hash}"
+
+    @classmethod
+    def from_str(cls, payload: str) -> PayloadHash:
+        return cls(payload.encode("utf-8"))
+
+    @classmethod
+    def from_hash(cls, hash_value: str) -> PayloadHash:
+        result = cls(b"")
+        tmp = cls._normalize_hash(hash_value)
+        if not bool(cls._SHA256_REGEX.fullmatch(tmp)):
+            raise ValueError(f"Hash value '{tmp}' is not a hex string")
+        result._hash = tmp
+        return result
