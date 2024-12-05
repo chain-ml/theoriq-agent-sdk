b45ce9cd443c93c05019e6fcf314b86978b05365
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2024-12-04 18:20:26 -0800

    clean up

--- theoriq/api/v1alpha2/configure.py
+++ theoriq/api/v1alpha2/configure.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable
 
 from theoriq import Agent
-from theoriq.api.v1alpha2 import AgentResponse, ProtocolClient
+from theoriq.api.v1alpha2 import ProtocolClient
 from theoriq.biscuit import AgentAddress
 
 
@@ -9,7 +9,6 @@ class ConfigureContext:
     def __init__(self, agent: Agent, protocol_client: ProtocolClient) -> None:
         self._agent = agent
         self._protocol_client = protocol_client
-        # self._request_biscuit = request_biscuit
 
     def set_virtual_address(self, address: str) -> None:
         self._agent.virtual_address = AgentAddress(address)
@@ -17,10 +16,6 @@ class ConfigureContext:
     @property
     def virtual_address(self) -> AgentAddress:
         return self._agent.virtual_address
-
-    @property
-    def virtual_agent(self) -> AgentResponse:
-        return self._protocol_client.get_agent(self._agent.virtual_address.address)
 
 
 ConfigureFn = Callable[[ConfigureContext, Any], None]
