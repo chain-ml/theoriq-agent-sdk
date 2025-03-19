eeb8138e462cc905c4b69899944e1e3dcf68f796
Farshad Kazemi <s.farshad.k@gmail.com>
2025-03-18 23:31:21 -0400

    feat: adds web3Results item

---
+++ theoriq/dialog/web3/web3_result.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from .. import Web3Item
+from typing import Dict, Any
+import json
+
+class Web3ResultItem(Web3Item):
+    """
+    A class representing the result of a Web3 item. Inherits from BaseData.
+    """
+
+    def __init__(self, *, chain_id: int, method: str, args: Dict[str, Any]) -> None:
+        """
+        Initializes a Web3Item instance.
+
+        Args:
+            chain_id (int): The chain_id that this web3 item is related to.
+            method (str): The method that this web3Item will execute.
+            args (List[str]): The arguments that this web3Item will execute with.
+        """
+        self.chain_id = chain_id
+        self.method = self.__class__.getWeb3Method()
+        self.args = args
+    
+    @staticmethod
+    def getWeb3Method() -> str:
+        return "result"
+
