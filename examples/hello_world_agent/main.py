a8018c3d4944c26bab2da99b59757afb89758c32
Farshad Kazemi <s.farshad.k@gmail.com>
2025-03-27 12:00:24 -0400

    revert: removes the agent examples and revert changes to the hello_world

--- examples/hello_world_agent/main.py
+++ examples/hello_world_agent/main.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 import os
 
@@ -9,7 +8,7 @@ from theoriq import AgentDeploymentConfiguration, ExecuteContext, ExecuteRespons
 from theoriq.api.v1alpha2.schemas import ExecuteRequestBody
 from theoriq.biscuit import TheoriqCost
 from theoriq.dialog import TextItemBlock, Web3EthSignBlock, Web3EthSignTypedDataBlock, Web3Item, Web3ItemBlock
-from theoriq.extra.flask.v1alpha2.flask import theoriq_blueprint_with_subscriber
+from theoriq.extra.flask.v1alpha2.flask import theoriq_blueprint
 from theoriq.types import Currency
 
 logger = logging.getLogger(__name__)
@@ -60,49 +59,7 @@ def execute(context: ExecuteContext, req: ExecuteRequestBody) -> ExecuteResponse
     )
 
 
-def subscribe(context: ExecuteContext, req: ExecuteRequestBody) -> None:
-    logger.info(
-        f"Received a new notification: {context.request_id} from {context.request_sender_type} {context.request_sender_address}"
-    )
-    logger.info(f"Received notification: {req}")
-
-
-# def publish_message(agent: Agent) -> Callable[[], Response]:
-#     def get_context() -> ExecuteContext:
-#         protocol_client = ProtocolClient.from_env()
-#         authentication_biscuit = agent.authentication_biscuit(
-#             expires_at=datetime.now(tz=timezone.utc) + timedelta(seconds=10)
-#         )
-#         agent_public_key = agent.config.public_key
-#         request_biscuit = protocol_client.get_biscuit(authentication_biscuit, agent_public_key)
-#         context = ExecuteContext(
-#             agent=agent,
-#             protocol_client= protocol_client,
-#             request_biscuit=request_biscuit,
-#         )
-#         return context
-
-#     def publish_message_inner() -> Response:
-#         context = get_context()
-#         logger.info(f"Received request: {request}")
-#         params = {}
-#         if request.is_json:
-#             params = request.get_json()  # Raw JSON data
-#         elif request.method == 'GET':
-#             params = request.args.to_dict()  # Query parameters
-#         else:
-#             params = request.form.to_dict()  # Form data
-
-#         if not request.is_json:
-#             return jsonify({"error": "Content-Type must be application/json"}), 415
-
-#         context.send_notification(json.dumps(params))
-#         return jsonify({"message": "Message published", "request": params})
-
-#     return publish_message_inner
-
-
-async def main():
+if __name__ == "__main__":
     app = Flask(__name__)
 
     # Logging
@@ -113,20 +70,6 @@ async def main():
     agent_config = AgentDeploymentConfiguration.from_env()
 
     # Create and register theoriq blueprint with v1alpha2 api version
-    blueprint, subscription_manager, agent = theoriq_blueprint_with_subscriber(agent_config, execute)
-
-    # blueprint.add_url_rule("/publish", view_func=publish_message(agent), methods=["POST"])
-    # Add a listener to the subscription manager
-    # publisher_agent_id = "0x0000000000000000000000000000000000000000"
-    # subscriber = subscription_manager.new_listener(subscribe, publisher_agent_id)
-    # subscriber.start_listener()
-
-    # await asyncio.sleep(10)  # Using asyncio.sleep instead of time.sleep
-    # subscriber.stop_listener()
-
+    blueprint = theoriq_blueprint(agent_config, execute)
     app.register_blueprint(blueprint)
     app.run(host="0.0.0.0", port=os.environ.get("FLASK_PORT", 8000))
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- examples/publisher/README.md
+++
@@ -1,2 +0,0 @@
-## Sample Publisher Agent
-Sample Agent that publishes a given string into its stream
