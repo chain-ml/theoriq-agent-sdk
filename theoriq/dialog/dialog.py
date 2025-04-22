4f0d293eb13e3a4d658660b41c1cf9d5bc2244c3
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2025-04-22 17:23:40 -0400

    Improving DialogItem - fix typos, etc.

--- theoriq/dialog/dialog.py
+++ theoriq/dialog/dialog.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import re
 from datetime import datetime, timezone
-from typing import Any, Callable, Dict, Iterable, List, Sequence, Type
+from typing import Any, Callable, Dict, Iterable, List, Mapping, Optional, Sequence, Type
 
 from pydantic import BaseModel, field_serializer, field_validator
 
@@ -18,7 +18,7 @@ from .runtime_error import ErrorItemBlock
 from .text import TextItemBlock
 from .web3 import Web3Item, Web3ItemBlock
 
-block_classes: Dict[str, Type[ItemBlock]] = {
+BLOCK_CLASSES_MAP: Mapping[str, Type[ItemBlock]] = {
     "code": CodeItemBlock,
     "custom": CustomItemBlock,
     "data": DataItemBlock,
@@ -35,22 +35,22 @@ class DialogItem:
     """
     A DialogItem object represents a message from a source during a dialog.
 
-    A single DialogItem contains multiple instances of DialogItemBlock. This allows an agent to send
-    multi-format responses for a single request. An agent, for example, can send Python and SQL code blocks
-    along with a markdown block.
+    A single DialogItem contains multiple instances of ItemBlock.
+    This allows an agent to send multi-format responses for a single request.
+    For example, it can send Python and SQL code blocks along with a Markdown text block.
 
     Attributes:
         timestamp (str): The creation time of the dialog item.
-        source (str): The creator of the dialog item. In the agent context, this is the agent's ID in the Theoriq protocol.
+        source (str): The creator of the dialog item. Either user address or Theoriq agent ID.
         source_type (str): The type of the source that creates the dialog item. Can be either 'user' or 'agent'.
-        blocks (list[ItemBlock]): A list of ItemBlock objects consisting of responses from the agent.
+        blocks (Sequence[ItemBlock]): A sequence of ItemBlock objects consisting of responses from the agent.
     """
 
     def __init__(self, timestamp: str, source_type: str, source: str, blocks: Sequence[ItemBlock[Any]]) -> None:
         self.timestamp: datetime = self._datetime_from_str(timestamp)
         self.source = source
         self.source_type = SourceType.from_value(source_type)
-        self.blocks = blocks
+        self.blocks = list(blocks)
 
     @classmethod
     def _datetime_from_str(cls, value: str) -> datetime:
@@ -72,17 +72,17 @@ class DialogItem:
             raise ValueError(f"Expect a dictionary, got {type(values)}")
 
         item_blocks: List[ItemBlock[Any]] = []
-        blocs = values.get("blocks", [])
-        for item in blocs:
+        blocks = values.get("blocks", [])
+        for item in blocks:
             block_type: str = item["type"]
-            bloc_class = block_classes.get(ItemBlock.root_type(block_type))
-            if bloc_class is not None:
-                block_data = item["data"]
-                block_key = item.get("key", None)
-                block_ref = item.get("ref", None)
-                item_blocks.append(bloc_class.from_dict(block_data, block_type, block_key, block_ref))
-            else:
-                raise ValueError(f"invalid item type {block_type}")
+            block_class = BLOCK_CLASSES_MAP.get(ItemBlock.root_type(block_type))
+            if block_class is None:
+                raise ValueError(f"Invalid item type {block_type}")
+
+            block_data = item["data"]
+            block_key = item.get("key", None)
+            block_ref = item.get("ref", None)
+            item_blocks.append(block_class.from_dict(block_data, block_type, block_key, block_ref))
 
         return cls(
             timestamp=values["timestamp"],
@@ -105,8 +105,28 @@ class DialogItem:
                 yield block
         return
 
+    def format_source(self, with_address: bool = True) -> str:
+        """Format the string describing the creator of the dialog item."""
+        # write unit tests
+        source_type = self.source_type.value.capitalize()
+        return source_type if not with_address else f"{source_type} ({self.source})"
+
+    def format(self, block_types: Optional[Sequence[Type[ItemBlock]]] = None) -> List[str]:
+        """Format all blocks in the current dialog item."""
+        # write unit tests
+        block_types = block_types or []
+
+        results: List[str] = []
+        for block in self.blocks:
+            for block_type in block_types:
+                if block_type.is_valid(block.block_type()):
+                    results.append(block.data.to_str())
+
+        return results
+
     @classmethod
     def new(cls, source: str, blocks: Sequence[ItemBlock[Any]]) -> DialogItem:
+        """Create a new instance with current datetime, deriving `source_type` from `source`."""
         return cls(
             timestamp=datetime.now(timezone.utc).isoformat(),
             source_type=SourceType.from_address(source).value,
@@ -116,27 +136,15 @@ class DialogItem:
 
     @classmethod
     def new_text(cls, source: str, text: str) -> DialogItem:
-        return cls(
-            timestamp=datetime.now(timezone.utc).isoformat(),
-            source_type=SourceType.from_address(source).value,
-            source=source,
-            blocks=[TextItemBlock(text)],
-        )
+        return DialogItem.new(source=source, blocks=[TextItemBlock(text)])
 
     @classmethod
     def new_route(cls, source: str, route: str, score: float) -> DialogItem:
-        return cls(
-            timestamp=datetime.now(timezone.utc).isoformat(),
-            source_type=SourceType.from_address(source).value,
-            source=source,
-            blocks=[RouterItemBlock([RouteItem(route, score)])],
-        )
+        return DialogItem.new(source=source, blocks=[RouterItemBlock([RouteItem(route, score)])])
 
     @classmethod
     def new_web3(cls, source: str, chain_id: int, method: str, args: Dict[str, Any]) -> DialogItem:
-        return cls(
-            timestamp=datetime.now(timezone.utc).isoformat(),
-            source_type=SourceType.from_address(source).value,
+        return DialogItem.new(
             source=source,
             blocks=[Web3ItemBlock(item=Web3Item(chain_id=chain_id, method=method, args=args))],
         )
