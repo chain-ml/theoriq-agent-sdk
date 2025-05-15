36ad703eb0cc585b60d4c76eda5986683da43d46
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2025-05-15 15:55:56 -0400

    Add unit tests for new functionality

--- tests/unit/test_agent_data.py
+++ tests/unit/test_agent_data.py
@@ -1,8 +1,88 @@
 import os.path
 
+import pytest
 from tests import DATA_DIR
 
-from theoriq.types import AgentDataObject
+from theoriq.types import (
+    AgentConfiguration,
+    AgentDataObject,
+    AgentMetadata,
+    DeploymentConfiguration,
+    Header,
+    VirtualConfiguration,
+)
+
+
+def test_metadata_camel_snake_case() -> None:
+    metadata_snake = AgentMetadata(
+        name="Test Agent",
+        short_description="Short description",
+        long_description="Long description",
+        tags=["tag"],
+        example_prompts=["prompt"],
+        cost_card="Free",
+        image_url="http://example.com/image.jpg",
+    )
+
+    metadata_camel = AgentMetadata(
+        **{  # type: ignore
+            "name": "Test Agent",
+            "shortDescription": "Short description",
+            "longDescription": "Long description",
+            "tags": ["tag"],
+            "examplePrompts": ["prompt"],
+            "costCard": "Free",
+            "imageUrl": "http://example.com/image.jpg",
+        }
+    )
+
+    assert metadata_snake == metadata_camel
+
+    assert metadata_snake.to_dict() == metadata_camel.to_dict()
+    assert all("_" not in key for key in metadata_snake.to_dict().keys())
+
+
+def test_agent_configuration() -> None:
+    config = AgentConfiguration(deployment=DeploymentConfiguration(url="http://example.com"))
+
+    dict_output = config.to_dict()
+    assert "deployment" in dict_output
+    assert "virtual" not in dict_output
+    assert dict_output["deployment"]["url"] == "http://example.com"
+    assert len(dict_output["deployment"]["headers"]) == 0
+
+    config = AgentConfiguration(
+        deployment=DeploymentConfiguration(headers=[Header(name="test", value="value")], url="http://example.com")
+    )
+
+    dict_output = config.to_dict()
+    assert "deployment" in dict_output
+    assert "virtual" not in dict_output
+    assert dict_output["deployment"]["url"] == "http://example.com"
+    assert len(dict_output["deployment"]["headers"]) == 1
+    assert dict_output["deployment"]["headers"][0]["name"] == "test"
+    assert dict_output["deployment"]["headers"][0]["value"] == "value"
+
+    config = AgentConfiguration(virtual=VirtualConfiguration(agent_id="0xabc", configuration={"key": "value"}))
+
+    dict_output = config.to_dict()
+    assert "deployment" not in dict_output
+    assert "virtual" in dict_output
+    assert dict_output["virtual"]["agentId"] == "0xabc"
+    assert dict_output["virtual"]["configuration"] == {"key": "value"}
+
+
+def test_agent_configuration_validation() -> None:
+    with pytest.raises(ValueError) as e:
+        AgentConfiguration(deployment=None, virtual=None)
+    assert "Exactly one of deployment or virtual must be provided" in str(e.value)
+
+    with pytest.raises(ValueError):
+        AgentConfiguration(
+            deployment=DeploymentConfiguration(url="http://example.com"),
+            virtual=VirtualConfiguration(agent_id="0xabc", configuration={}),
+        )
+    assert "Exactly one of deployment or virtual must be provided" in str(e.value)
 
 
 def test_agent_data_parent() -> None:
