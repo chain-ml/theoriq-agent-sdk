61ce503f5d1af36f2548b9b35b1f8f1123846b65
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2025-06-18 17:07:06 -0400

    test: must_read_env_*

--- tests/unit/test_utils_env.py
+++ tests/unit/test_utils_env.py
@@ -5,6 +5,10 @@ import pytest
 from theoriq.utils import (
     EnvVariableValueException,
     MissingEnvVariableException,
+    must_read_env_bool,
+    must_read_env_float,
+    must_read_env_int,
+    must_read_env_str,
     read_env_bool,
     read_env_float,
     read_env_int,
@@ -14,33 +18,33 @@ from theoriq.utils import (
 
 def test_read_env_str() -> None:
     os.environ["STR"] = "VALUE"
-    value = read_env_str("STR", required=True)
+    value = must_read_env_str("STR")
     assert isinstance(value, str)
     assert value == "VALUE"
 
 
 def test_read_env_int() -> None:
     os.environ["INT"] = "2"
-    value = read_env_int("INT", required=True)
+    value = must_read_env_int("INT")
     assert isinstance(value, int)
     assert value == 2
 
 
 def test_read_env_float() -> None:
     os.environ["FLOAT"] = "1.23456"
-    value = read_env_float("FLOAT", required=True)
+    value = must_read_env_float("FLOAT")
     assert isinstance(value, float)
     assert value == 1.23456
 
 
 def test_read_env_bool() -> None:
     os.environ["T_BOOL"] = "True"
-    value = read_env_bool("T_BOOL", required=True)
+    value = must_read_env_bool("T_BOOL")
     assert isinstance(value, bool)
     assert value
 
     os.environ["F_BOOL"] = "False"
-    value = read_env_bool("F_BOOL", required=True)
+    value = must_read_env_bool("F_BOOL")
     assert isinstance(value, bool)
     assert not value
 
@@ -52,11 +56,20 @@ def test_missing_env_with_default() -> None:
 
 
 def test_missing_env() -> None:
-    value = read_env_str("MISSING")
-    assert value is None
+    str_value = read_env_str("MISSING")
+    assert str_value is None
+
+    int_value = read_env_int("MISSING")
+    assert int_value is None
+
+    float_value = read_env_float("MISSING")
+    assert float_value is None
+
+    bool_value = read_env_bool("MISSING")
+    assert bool_value is None
 
     with pytest.raises(MissingEnvVariableException):
-        read_env_str("MISSING", required=True)
+        must_read_env_str("MISSING")
 
 
 def test_invalid_env() -> None:
@@ -66,4 +79,4 @@ def test_invalid_env() -> None:
 
     os.environ["INVALID_BOOL"] = "Tue"
     with pytest.raises(EnvVariableValueException):
-        read_env_bool("INVALID_BOOL")
+        must_read_env_bool("INVALID_BOOL")
