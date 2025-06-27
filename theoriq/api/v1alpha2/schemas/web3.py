c635348b1231ce5dc6b2e60fba29e1fdd805b50b
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2025-06-27 15:47:02 -0400

    feat: use alias_generator

--- theoriq/api/v1alpha2/schemas/web3.py
+++ theoriq/api/v1alpha2/schemas/web3.py
@@ -1,12 +1,15 @@
 from typing import Dict, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class AgentWeb3Transaction(BaseModel):
-    agent_id: str = Field(..., alias="agentId")
-    chain_id: int = Field(..., alias="chainId")
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    agent_id: str
+    chain_id: int
     hash: str
     metadata: Optional[Dict[str, str]] = None
     signer: str
-    submitted_at: str = Field(..., alias="submittedAt")
+    submitted_at: str
