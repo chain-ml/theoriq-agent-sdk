2aa66ff5577bd6ee3bb709fa1e469008a59352b1
Nikolaiev Dmytro <nikolaevdmitryprog@gmail.com>
2025-07-11 15:29:11 -0400

    Move last_item functionality from ExecuteRequestBody to Dialog (#122)
    * feat: move last_item functionality from ExecuteRequestBody to Dialog
    
    * feat: keep methods in execute body for backward compatability

--- theoriq/dialog/dialog.py
+++ theoriq/dialog/dialog.py
@@ -210,6 +210,65 @@ class Dialog(BaseModel):
 
     items: Sequence[DialogItem]
 
+    @property
+    def last_item(self) -> Optional[DialogItem]:
+        """
+        Returns the last dialog item contained in the request based on the timestamp.
+
+        Returns:
+            Optional[DialogItem]: The dialog item with the most recent timestamp, or None if there are no items.
+        """
+        if len(self.items) == 0:
+            return None
+
+        return max(self.items, key=lambda obj: obj.timestamp)
+
+    @property
+    def last_text(self) -> str:
+        """
+        Returns the last text item from the dialog.
+
+        Returns:
+            str: The last text item from the dialog.
+
+        Raises:
+            RuntimeError: If the dialog is empty or no text blocks are found in the last dialog item.
+        """
+        last_item = self.last_item
+        if last_item is None:
+            raise RuntimeError("Got empty dialog")
+
+        return last_item.extract_last_text()
+
+    def last_item_from(self, source_type: SourceType) -> Optional[DialogItem]:
+        """
+        Returns the last dialog item from a specific source type based on the timestamp.
+
+        Args:
+            source_type (SourceType): The source type to filter the dialog items.
+
+        Returns:
+            Optional[DialogItem]: The dialog item with the most recent timestamp from the specified source type,
+                                  or None if no items match the source type.
+        """
+
+        return self.last_item_predicate(lambda item: item.source_type == source_type)
+
+    def last_item_predicate(self, predicate: DialogItemPredicate) -> Optional[DialogItem]:
+        """
+        Returns the last dialog item that matches the given predicate based on the timestamp.
+
+        Args:
+            predicate (DialogItemPredicate): A function that takes a DialogItem and returns a boolean.
+
+            Returns:
+                Optional[DialogItem]: The dialog item that matches the predicate and has the latest timestamp,
+                                       or None if no items match the predicate.
+        """
+
+        items = (item for item in self.items if predicate(item))
+        return max(items, key=lambda obj: obj.timestamp) if items else None
+
     def map(self, func: DialogItemTransformer) -> List[Any]:
         """Apply a function to each item in the dialog."""
         return [func(item) for item in self.items]
