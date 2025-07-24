bc2ce409339b79f2f9611f84b4954f5a2dc82659
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2025-07-24 12:16:36 -0700

    rename to biscuit_request_id

--- theoriq/extra/logging/execute_context.py
+++ theoriq/extra/logging/execute_context.py
@@ -4,16 +4,16 @@ from typing import ContextManager, Optional
 from theoriq.api.common import ExecuteContextBase
 from .http_request_context import x_request_id_var
 
-request_id_var: ContextVar[Optional[str]] = ContextVar("theoriq_request_id", default=None)
+biscuit_request_id: ContextVar[Optional[str]] = ContextVar("theoriq_request_id", default=None)
 
 
 class ExecuteLogContext(ContextManager):
     def __init__(self, context: ExecuteContextBase, request_id_header: Optional[str] = None) -> None:
-        self._token = request_id_var.set(context.request_id)
+        self._token = biscuit_request_id.set(context.request_id)
         self._header_token =  x_request_id_var.set(request_id_header) if request_id_header else None
 
     def __exit__(self, exc_type, exc_value, traceback, /):
-        request_id_var.reset(self._token)
+        biscuit_request_id.reset(self._token)
         if self._header_token:
             x_request_id_var.reset(self._header_token)
 
@@ -21,7 +21,7 @@ class ExecuteLogContext(ContextManager):
 def get_record_factory(old_factory):
     def record_factory(*args, **kwargs):
         record = old_factory(*args, **kwargs)
-        record.theoriq_request_id = request_id_var.get()
+        record.theoriq_request_id = biscuit_request_id.get()
         return record
 
     return record_factory
