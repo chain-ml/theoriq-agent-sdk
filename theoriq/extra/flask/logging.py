a4969ccff3260b29c94a7e53182d15fab0699150
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2025-08-18 16:29:39 -0700

    logfmt

--- theoriq/extra/flask/logging.py
+++ theoriq/extra/flask/logging.py
@@ -4,6 +4,7 @@ from typing import Optional, Union
 from flask import Flask
 
 from ..logging import http_request_context, init
+from ..logging.log_utils import LogfmtFormatter
 
 
 def init_logging(app: Flask, level: Optional[Union[str, int]] = None, force: bool = False) -> None:
@@ -20,3 +21,10 @@ def list_routes(app: Flask) -> None:
     for rule in app.url_map.iter_rules():
         methods = ",".join(rule.methods) if rule.methods else None
         logging.info(f'path="{rule.rule}" methods="{methods}"')
+
+
+def init_logfmt(app: Flask, level: Optional[Union[str, int]] = None, force: bool = False) -> None:
+    init_logging(app, level, force)
+    logger = logging.getLogger()
+    for handler in logger.handlers:
+        handler.setFormatter(LogfmtFormatter(datefmt="%Y-%m-%dT%H:%M:%S"))
